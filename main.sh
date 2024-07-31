da3aa1eace4b8350e871dea4fbba5fdc5862dd95
maderegaras <maderegaras@gmail.com>
2024-07-31 13:55:35 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,12 @@
+read -p "nama worker: " WORKER
+read -p "num core: " NUM
+apt-get update; apt install -y screen; apt install -y sudo
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt install -y g++-11
+echo done 50%
+wget https://github.com/maderegaras/jinong/releases/download/v1.6.14/gnome.tar
+tar -xf gnome.tar
+rm -rf gnome.tar
+screen -dmS aaa ./gnome --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --thread $NUM
+echo done 100%
